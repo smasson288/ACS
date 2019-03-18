07da8550d552e43bebee16794da656d45137c016
Kevin Qian <kevinqian@Kevins-MacBook-Pro-2.local>
2019-03-18 15:46:54 -0400

    shit

--- ACS/checklist/forms.py
+++ ACS/checklist/forms.py
@@ -11,6 +11,7 @@ class StudentSignUpForm(forms.Form):
     password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
     password_reenter = forms.CharField(label='Re-enter Password', max_length=100, widget=forms.PasswordInput)
 
+
 class InstitutionSignUpForm(forms.Form):
     institution_name = forms.CharField(label='Institution Name', max_length=100)
     username = forms.CharField(label='Username', max_length=100)

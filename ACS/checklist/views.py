15bcc9e69da54c988d0d6a3c8c75e39a108cc85c
Siying Wang <siying.wang3@case.edu>
2019-03-18 22:49:45 -0400

    add program requirement to checklist

--- ACS/checklist/views.py
+++ ACS/checklist/views.py
@@ -52,11 +52,11 @@ def studentCreateAccount(request):
             username_count = len(Student.objects.filter(username=username))
             if username_count != 0:
                 messages.warning(request, 'username is already taken')
-                return render(request, 'studentCreateAccount.html')
+                return render(request, 'studentCreateAccount.html', {'form': form})
 
             if password != reenter:
                 messages.warning(request, 'passwords does not match')
-                return render(request, 'studentCreateAccount.html')
+                return render(request, 'studentCreateAccount.html', {'form': form})
 
             user = Student.objects.create_user(username, password)
             user.save()
@@ -67,18 +67,57 @@ def studentCreateAccount(request):
     return render(request, 'studentCreateAccount.html', {'form': form})
 
 def staffCreateAccount(request):
-    return render(request, 'staffCreateAccount.html')
+    if request.method == 'POST':
+        form = InstitutionSignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            reenter = form.cleaned_data['password_reenter']
+            school_name = form.cleaned_data['institution_name']
 
+            username_count = len(Staff.objects.filter(username=username))
+            if username_count != 0:
+                messages.warning(request, 'username is already taken')
+                return render(request, 'staffCreateAccount.html', {'form': form})
 
+            if password != reenter:
+                messages.warning(request, 'passwords does not match')
+                return render(request, 'staffCreateAccount.html', {'form': form})
+
+            try:
+                school = School.objects.get(School_name=school_name)
+                school_id = school.School_id
+            except School.DoesNotExist:
+                last_school = School.objects.filter().order_by('School_id').last()
+                if last_school is not None:
+                    school_id = last_school.School_id + 1
+                else:
+                    school_id = 0
+                school = School(School_id=school_id, School_name=school_name)
+                school.save()
+
+            user = Staff.objects.create_user(username, password, school_id)
+            user.save()
+
+            return HttpResponseRedirect('/login/')
+    else:
+        form = InstitutionSignUpForm()
+    return render(request, 'staffCreateAccount.html', {'form': form})
+
+@csrf_protect
 def checklist(request):
     if request.user.is_authenticated:
         username = request.user.username
         user_model = get_object_or_404(Student, pk=username)
-        checklists = Checklist.objects.filter(Student_id=user_model.username)
-        return render(request, 'checklist.html', {'user': user_model, 'checklists': checklists})
+        checklists = Checklist.objects.filter(Student_id=username)
+        requirements = []
+
+        for list in checklists:
+            requirements.append([list, Requirement.objects.get(Requirement_id=list.Requirement_id.Requirement_id)])
+
+        return render(request, 'checklist.html', {'user': user_model, 'checklists': requirements})
     else:
         return HttpResponseRedirect('/login/')
-
 
 def search(request):
     if request.method == 'POST':
@@ -90,7 +129,7 @@ def search(request):
 
             program_list = []
             programs = Program.objects.filter(Degree__contains=degree, Major__contains=major, School_id__School_name__contains=university).all()
-            return render(request, 'search.html', {'programs': programs})
+            return render(request, 'search.html', {'form':form,'programs':programs})
 
     form = ProgramSearchForm()
 
@@ -100,13 +139,33 @@ def search(request):
 def program(request):
     return render(request, 'program.html')
 
-
+@csrf_protect
 def programDetail(request, program_id):
-    currentProgram = get_object_or_404(Program, pk=program_id)
-    Requirements = Requirement.objects.filter(Program_id=program_id)
-    context = {'program': currentProgram, 'requirements': Requirements}
-    return render(request, 'programDetail.html', context)
+    if request.method == 'POST':
+        # if add to checklist:
+        checklist = Checklist.objects.filter(Student_id=request.user, Requirement_id__Program_id__Program_id=program_id)
 
+        if len(checklist) is 0:
+            last_checklist = Checklist.objects.filter().last()
+            if last_checklist is None:
+                checklist_id = 0
+            else:
+                checklist_id = last_checklist.Checklist_id + 1
+
+            requirement = Requirement.objects.filter(Program_id=program_id).last()
+            checklist = Checklist(checklist_id, requirement.Requirement_id, request.user, requirement.Term_season,
+                                  requirement.Term_year, requirement.Recommendation_letters,
+                                  requirement.Transcript, requirement.Tests, requirement.Statement_of_purpose,
+                                  requirement.Personal_statement)
+            checklist.save()
+
+        return HttpResponseRedirect('/checklist/')
+
+    currentProgram = get_object_or_404(Program, pk=program_id)
+    requirements = Requirement.objects.filter(Program_id=program_id)
+    context = {'program': currentProgram, 'requirements': requirements}
+
+    return render(request, 'programDetail.html', context)
 
 def createProgram(request):
     if request.method == 'POST':
@@ -132,6 +191,7 @@ def createProgram(request):
                     school_id = 0
                 school = School(School_id=school_id, School_name=university_name)
                 school.save()
+
 
             program = Program(Program_id=program_id, Major=form.cleaned_data['major'], Degree=form.cleaned_data['degree_type'], School_id_id=school_id)
             program.save()
